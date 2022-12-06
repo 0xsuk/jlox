70733a4547d6e750ed421751a0da27bea11f9341
0xsuk <97814789+0xsuk@users.noreply.github.com>
2022-12-06 18:30:43 +0900

    fix BinaryExpr interpreter bug

--- src/main/java/com/craftinginterpreters/lox/Interpreter.java
+++ src/main/java/com/craftinginterpreters/lox/Interpreter.java
@@ -117,8 +117,8 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
 	@Override
 	public Object visitBinaryExpr(Expr.Binary expr) {
-		Object left = evaluate(expr);
-		Object right = evaluate(expr);
+		Object left = evaluate(expr.left);
+		Object right = evaluate(expr.right);
 
 		switch (expr.operator.type) {
 			case GREATER:
