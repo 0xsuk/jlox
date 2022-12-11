165d895febc8fbd80ec38270a013460844c32406
0xsuk <97814789+0xsuk@users.noreply.github.com>
2022-12-11 13:33:48 +0900

    function return statement

--- src/main/java/com/craftinginterpreters/lox/LoxFunction.java
+++ src/main/java/com/craftinginterpreters/lox/LoxFunction.java
@@ -18,8 +18,12 @@ class LoxFunction implements LoxCallable {
 					arguments.get(i));
 		}
 
-		interpreter.executeBlock(declaration.body, environment);
-		return null;
+		try {
+			interpreter.executeBlock(declaration.body, environment);
+		} catch (Return returnValue) {
+			return returnValue.value;
+		}
+		return null; // every function without return value returns nil
 	}
 
 	@Override
